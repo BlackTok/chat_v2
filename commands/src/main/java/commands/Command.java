a5456a25462cafbed5e213ac185b4b8d8eff48be
BlackTok <nu.pomytkin@gmail.com>
2021-02-02 14:50:51 +0300

    Lesson 7

--- commands/src/main/java/commands/Command.java
+++ commands/src/main/java/commands/Command.java
@@ -5,4 +5,8 @@ public class Command {
     public static final String AUTH = "/auth";
     public static final String AUTH_OK = "/authok";
 
+    public static final String PRIVATE_MSG = "/w";
+
+    public static final String ERROR_MSG_UNKNOWN_USER = "Такого пользователя не существует";
+    public static final String ERROR_MSG_SEND_YOURSELF = "Вы пытаетесь отправить сообщение самому себе";
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,12 @@
 package server;
 
+import commands.Command;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
@@ -42,6 +45,27 @@ public class Server {
         String message = String.format("[ %s ] : %s", sender.getNickname(), msg);
         for (ClientHandler c : clients) {
             c.sendMsg(message);
+        }
+    }
+    
+    public void broadcastPrivateMsg (ClientHandler sender, String recipientName, String msg) {
+        String message = String.format("[ %s ] : %s", sender.getNickname(), msg);
+        ClientHandler recipient = null;
+
+        for (ClientHandler client : clients) {
+            if (client.getNickname().equals(recipientName)) {
+                recipient = client;
+                break;
+            }
+        }
+
+        if (recipient == null) {
+            sender.sendMsg(Command.ERROR_MSG_UNKNOWN_USER);
+        } else if (recipient.equals(sender)) {
+            sender.sendMsg(Command.ERROR_MSG_SEND_YOURSELF);
+        } else {
+            sender.sendMsg(message);
+            recipient.sendMsg(message);
         }
     }
 
